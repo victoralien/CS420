c83179d59880d5a9eb4314fdb0ef070b7143acf5
victoralien <tallen812victor@gmail.com>
2024-10-29 15:57:45 -0700

    Create main.cpp

---
+++ GroupProject/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  GroupProject
+//
+//  Created by Victor Allen on 10/28/24.
+//
+
+#include <iostream>
+#include "Game.h"
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    cout << "Welcome to blackjack!\n";
+    
+    char again = 'y';
+    
+    string name;
+    
+    cout << "Enter name: ";
+    getline(cin,name);
+    
+    Game g(name);
+    
+    while(again == 'y' || again == 'Y'){
+        g.play();
+        
+        cout << "Thank you for playing!\nPlay Again? (y/n): ";
+        cin >> again;
+    }
+    
+    cout << "\nGoodbye!";
+   
+}
